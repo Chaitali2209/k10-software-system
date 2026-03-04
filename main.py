7ce04010978fb85b4d2c56fdaa8193156806c2fe
rudrakatkar <rudrakatkar9@gmail.com>
2026-03-04 17:48:09 +0530

    implemented multi window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from ui.logs_window import LogsWindow
+from video.video_worker import VideoWorker
+from ui.controller_window import PlainVideoWindow
 from ui.main_window import MainWindow
 
 if __name__ == "__main__":
@@ -8,6 +11,24 @@ if __name__ == "__main__":
     with open("ui/theme.qss", "r") as f:
         app.setStyleSheet(f.read())
 
-    win = MainWindow()
-    win.show()
+
+    worker = VideoWorker(device_id=1)
+
+    # third window for technical logs
+    logs_win = LogsWindow()
+    logs_win.move(100, 100)
+    logs_win.show()
+
+    # first window controller
+    plain_win = PlainVideoWindow(worker)
+    plain_win.move(150, 150)
+    plain_win.show()
+
+    # second window for AI inference feed
+    ai_win = MainWindow(worker)
+    ai_win.move(200, 200)
+    ai_win.show()
+
+    print("[SYSTEM] K-10 Software System Started Successfully")
+
     sys.exit(app.exec())
